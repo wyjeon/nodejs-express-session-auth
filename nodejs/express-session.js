6fc9e199d2641a10e087ba9747f4508a491aaa9e
wyjeon <wyjeon.info@gmail.com>
2021-01-30 14:07:02 +0900

    5. session store

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var parseurl = require("parseurl");
 var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
 var app = express();
 
@@ -9,6 +10,7 @@ app.use(
     secret: "keyboard cat",
     resave: false, // 세션데이터가 바뀌기 전 까지 세션데이터를 저장하지 않는다.
     saveUninitialized: true, // 세션 필요하기 전 까지 세션을 구동하지 않는다.
+    store: new FileStore(),
   })
 );
 
