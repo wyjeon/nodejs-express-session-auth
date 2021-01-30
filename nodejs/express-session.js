ccd60a1c6177bc1864dd3ee6bbad1b1b8f8b6f9f
wyjeon <wyjeon.info@gmail.com>
2021-01-30 14:01:37 +0900

    4. session 객체

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -13,7 +13,13 @@ app.use(
 );
 
 app.get("/", function (req, res, next) {
-  res.send("Hello session");
+  console.log(req.session);
+  if (req.session.num === undefined) {
+    req.session.num = 1;
+  } else {
+    req.session.num = req.session.num + 1;
+  }
+  res.send(`Views : ${req.session.num}`);
 });
 
 app.listen(3000, function () {
