3e2b96318a8a06718523bc6f4f82d869c6557cf6
wyjeon <wyjeon.info@gmail.com>
2021-01-30 14:27:54 +0900

    6.3. 인증구현 - 세션 미들웨어 설치

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ var bodyParser = require("body-parser");
 var compression = require("compression");
 var helmet = require("helmet");
 app.use(helmet());
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore(),
+  })
+);
+
 app.get("*", function (request, response, next) {
   fs.readdir("./data", function (error, filelist) {
     request.list = filelist;
--- routes/auth.js
+++ routes/auth.js
@@ -37,11 +37,12 @@ router.post("/login_process", function (request, response) {
   var email = post.email;
   var password = post.pwd;
   if (email === authData.email && password === authData.password) {
-    response.send("Welcome!");
+    request.session.is_logined = true;
+    request.session.nickname = authData.nickname;
+    response.redirect(`/`);
   } else {
     response.send("Who?");
   }
-  //response.redirect(`/topic/${title}`);
 });
 
 module.exports = router;
